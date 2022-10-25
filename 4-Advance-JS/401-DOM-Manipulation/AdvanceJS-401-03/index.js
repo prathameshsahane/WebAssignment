e4e75435871f9e08ab04e271f1b79c222ae9f192
Sakshi Shinde <you@example.comsahanep3@gmail.com>
2022-10-25 21:10:39 +0530

    advanced js assignments

---
+++ 4-Advance-JS/401-DOM-Manipulation/AdvanceJS-401-03/index.js
@@ -0,0 +1,22 @@
+let flag=0;
+function controller(x){
+    flag+=x;
+    slideshow(flag);
+}
+slideshow(flag);
+
+function slideshow(num){
+    let slides=document.getElementsByClassName('slide');
+    if(num==slides.length){
+        num=0;
+        flag=0;
+    }
+    if(num==-1){
+        num=slides.length-1;
+        flag=
+    }
+    for(let y of slides){
+        y.style.display="none";
+    }
+    slides[num].style.display="block";
+}
